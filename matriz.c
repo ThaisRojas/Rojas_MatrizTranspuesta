af12992b8c31e8018e3da368d9d91f4d2778ddbf
Thais Rojas <thais.rojas@udla.edu.ec>
2023-11-23 16:17:14 -0500

    inicio numeros aleatorios

---
+++ matriz.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void MatrizA(int filas, int columnas, int matriz[filas][columnas]) {
+    int i, j;
+    srand(time(NULL)); // Inicializa los números aleatorios
+}
